1845bc199e0cd28f6e536aca14637655390a377f
Sagar Chaki <chaki@sei.cmu.edu>
2013-11-26 17:50:26 -0500

    added command to set VREP_HOME.

--- examples/setenv.sh
+++ examples/setenv.sh
@@ -2,3 +2,4 @@ export ACE_ROOT=/home/chaki/stuff/madara/ace/ACE_wrappers
 export MADARA_ROOT=/home/chaki/stuff/madara/madara
 export LD_LIBRARY_PATH=$ACE_ROOT/lib:$MADARA_ROOT/lib:$LD_LIBRARY_PATH
 export PATH=$ACE_ROOT/bin:$MADARA_ROOT/bin:$PATH
+export VREP_HOME=/home/chaki/stuff/vrep/V-REP_PRO_EDU_V3_0_4_64_Linux
