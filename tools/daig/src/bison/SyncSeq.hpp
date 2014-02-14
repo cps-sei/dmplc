044339eff12b2494f463db4a525f848b2f456d88
Sagar Chaki <chaki@sei.cmu.edu>
2014-02-14 18:19:07 -0500

    files renamed. new SyncSeq.cpp and SyncSeq.hpp files to implement synchronous sequentialization.

---
+++ tools/daig/src/bison/SyncSeq.hpp
@@ -0,0 +1,12 @@
+//a class for sequentializing DAIG into a C program
+
+#ifndef __SYNC_SEQ_HPP__
+#define __SYNC_SEQ_HPP__
+
+namespace daig {
+  class SyncSeq
+  {
+  };
+} //namespace daig
+
+#endif //__SYNC_SEQ_HPP__
