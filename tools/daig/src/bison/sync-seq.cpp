e78e3e7b935ba6c6dcdd44fb7d8843fcb6f25637
Sagar Chaki <chaki@sei.cmu.edu>
2014-02-14 14:45:38 -0500

    starting on the sequentializer for synchronous model of computation. right now this just parses the file and prints it out.

---
+++ tools/daig/src/bison/sync-seq.cpp
@@ -0,0 +1,37 @@
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include "DaigBuilder.hpp"
+
+//options
+std::string fileName;
+bool debug;
+
+void parseOptions(int argc, char **argv);
+
+int main(int argc, char **argv)
+{
+  parseOptions(argc,argv);
+
+  //create the program
+  daig::DaigBuilder db(fileName,debug);
+  db.run();
+
+  //all done
+  return 0;
+}
+
+void parseOptions(int argc, char **argv)
+{
+  for(int i = 1;i < argc;++i) {
+    if(!strcmp(argv[i],"--debug")) debug = true;
+    else fileName = std::string(argv[i]);
+  }
+
+  if(fileName.empty()) {
+    std::cerr << "ERROR: no filename ...\n";
+    std::cerr << "Usage : " << argv[0] << " filename [--debug]\n";
+    exit(1);
+  }
+}
