7b7fafac6094f79f33251cf97c5dd1f917302077
Sagar Chaki <chaki@sei.cmu.edu>
2014-02-18 07:41:12 -0500

    made script more portable. looking for cbmc and plingeling on PATH instead of having hard coded paths in the script.

--- tools/daig/src/bison/cbmc-plingeling.sh
+++ tools/daig/src/bison/cbmc-plingeling.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 TMPF=$(mktemp)
 echo "CNF file = "$TMPF.cnf
-/home/chaki/stuff/cbmc/cbmc-4.7/cbmc --dimacs --outfile $TMPF.cnf $*
-/home/chaki/stuff/plingeling/plingeling-aqw-27d9fd4-130429/binary/plingeling -t 4 $TMPF.cnf
+
+function ensure {
+    X=$(which $1)
+    if [ x"$X" == "x" ]; then
+        echo "$1 not found, make sure it is on your path"
+        exit 1
+    fi
+}
+
+ensure cbmc
+ensure plingeling
+
+cbmc --dimacs --outfile $TMPF.cnf $*
+plingeling -t 4 $TMPF.cnf
