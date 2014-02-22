d447116080e0cf60232fcced18047bc86fb6720e
Sagar Chaki <chaki@sei.cmu.edu>
2014-02-22 12:38:06 -0500

    renamed files: .mcda ==> .dasl and added .ok to the synchronous collision avoidance program to indicate that it is correct.
    updated regression script so that it works with the new file names and
    the relative location of the daslc program.

--- examples/regress.sh
+++ examples/regress.sh
@@ -2,11 +2,13 @@
 TMPF1=$(mktemp)
 TMPF2=$(mktemp)
 
+DASLC=../tools/daig/src/daslc/daslc
+
 echo "testing daslc parsing and printing"
-for i in ../../../../examples/sync-coll-avoid.mcda \
-    ../../../../examples/async-coll-avoid.mcda; do 
-    ./daslc $i --print > $TMPF1
-    ./daslc $TMPF1 --print > $TMPF2
+for i in sync-coll-avoid.ok.dasl \
+    async-coll-avoid.dasl; do 
+    $DASLC $i --print > $TMPF1
+    $DASLC $TMPF1 --print > $TMPF2
     DIFF=$(diff $TMPF1 $TMPF2 | wc -l)
     if [ "$DIFF" == "0" ]; then
         echo "$(basename $i) PASSED"
