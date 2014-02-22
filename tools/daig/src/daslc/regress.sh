899f51d336128b80d0e515c5960be1b6a5359874
Sagar Chaki <chaki@sei.cmu.edu>
2014-02-22 12:27:50 -0500

    improved output. prints whether each test failed or passed.

--- tools/daig/src/daslc/regress.sh
+++ tools/daig/src/daslc/regress.sh
@@ -2,10 +2,15 @@
 TMPF1=$(mktemp)
 TMPF2=$(mktemp)
 
+echo "testing daslc parsing and printing"
 for i in ../../../../examples/sync-coll-avoid.mcda \
     ../../../../examples/async-coll-avoid.mcda; do 
-    echo "== $i"
     ./daslc $i --print > $TMPF1
     ./daslc $TMPF1 --print > $TMPF2
-    diff $TMPF1 $TMPF2
+    DIFF=$(diff $TMPF1 $TMPF2 | wc -l)
+    if [ "$DIFF" == "0" ]; then
+        echo "$(basename $i) PASSED"
+    else
+        echo "$(basename $i) FAILED"
+    fi
 done
