48b6564e21d8fbb8af6b14f75f97940a29050306
James Edmondson <jedmondson@gmail.com>
2014-03-02 05:06:21 -0500

    * Added CMU license to each of the daig source and header files * Added exists higher, exists lower, and exists other to the MADARA generation * Added break and continue handlers * Fixed some issues with multi-dimensional array references * Added id map to Function_Visitor to accomodate temporary variables within daig commands

--- tools/daig/src/daig/madara/Function_Visitor.cpp
+++ tools/daig/src/daig/madara/Function_Visitor.cpp
@@ -1,3 +1,48 @@
+/**
+ * Copyright (c) 2013 Carnegie Mellon University. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following acknowledgments and disclaimers.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. The names “Carnegie Mellon University,” "SEI” and/or “Software
+ *    Engineering Institute" shall not be used to endorse or promote products
+ *    derived from this software without prior written permission. For written
+ *    permission, please contact permission@sei.cmu.edu.
+ * 
+ * 4. Products derived from this software may not be called "SEI" nor may "SEI"
+ *    appear in their names without prior written permission of
+ *    permission@sei.cmu.edu.
+ * 
+ * 5. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ * 
+ *      This material is based upon work funded and supported by the Department
+ *      of Defense under Contract No. FA8721-05-C-0003 with Carnegie Mellon
+ *      University for the operation of the Software Engineering Institute, a
+ *      federally funded research and development center. Any opinions,
+ *      findings and conclusions or recommendations expressed in this material
+ *      are those of the author(s) and do not necessarily reflect the views of
+ *      the United States Department of Defense.
+ * 
+ *      NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING
+ *      INSTITUTE MATERIAL IS FURNISHED ON AN “AS-IS” BASIS. CARNEGIE MELLON
+ *      UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR
+ *      IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF
+ *      FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS
+ *      OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES
+ *      NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT,
+ *      TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+ * 
+ *      This material has been approved for public release and unlimited
+ *      distribution.
+ **/
 #include "Function_Visitor.hpp"
 #include "boost/foreach.hpp"
 
@@ -40,11 +85,13 @@ daig::madara::Function_Visitor::exitLval (LvalExpr & expression)
   {
     buffer_ << expression.var;
 
+    // indexing into a multi-dimensional array requires a different operator
     if (expression.indices.size () > 1)
       buffer_ << "(";
     else
       buffer_ << "[";
 
+    // iterate over each index
     bool started = false;
     BOOST_FOREACH (Expr & expr, expression.indices)
     {
@@ -52,6 +99,9 @@ daig::madara::Function_Visitor::exitLval (LvalExpr & expression)
         buffer_ << ", ";
 
       visit (expr);
+
+      if (!started)
+        started = true;
     }
 
     if (expression.indices.size () > 1)
@@ -67,11 +117,19 @@ daig::madara::Function_Visitor::exitLval (LvalExpr & expression)
   }
   else
   {
-    if (function_.temps.find (expression.var) == function_.temps.end ())
+    // if this is substitution variable, do the substitution
+    if (id_map_.find (expression.var) != id_map_.end ())
+      buffer_ << id_map_[expression.var];
+
+    // otherwise, we need to determine if this is a MADARA container or not
+    else
     {
-      buffer_ << "*";
+      if (function_.temps.find (expression.var) == function_.temps.end ())
+      {
+        buffer_ << "*";
+      }
+      buffer_ << expression.var;
     }
-    buffer_ << expression.var;
   }
 }
 
@@ -139,9 +197,46 @@ daig::madara::Function_Visitor::enterEXO (EXOExpr & expression)
 void
 daig::madara::Function_Visitor::exitEXO (EXOExpr & expression)
 {
-  std::string spacer (indentation_, ' ');
+  std::string spacer (indentation_, ' '), sub_spacer (indentation_ + 2, ' ');
 
-  buffer_ << spacer << "Handling exists other block.\n";
+  unsigned int processes = builder_.program.processes.size ();
+  
+  bool started_i = false;
+  for (unsigned int i = 0; i < processes; ++i)
+  {
+    if (started_i)
+    {
+      buffer_ << " || \n" << sub_spacer;
+    }
+
+    buffer_ << "(";
+    buffer_ << "*id == " << i << " && (";
+
+    bool started_j = false;
+    for (unsigned int j = 0; j < processes; ++j)
+    {
+      // we want to check others for existence, not ourselves
+      if (i == j)
+        continue; 
+
+      if (started_j)
+        buffer_ << " || ";
+
+      id_map_ [expression.id] = j;
+      
+      visit (expression.arg);
+
+      id_map_.erase (expression.id);
+
+      if (!started_j)
+        started_j = true;
+    }
+
+    buffer_ << "))";
+
+    if (!started_i)
+      started_i = true;
+  }
 }
 
 
@@ -155,9 +250,42 @@ daig::madara::Function_Visitor::enterEXH (EXHExpr & expression)
 void
 daig::madara::Function_Visitor::exitEXH (EXHExpr & expression)
 {
-  std::string spacer (indentation_, ' ');
+  std::string spacer (indentation_, ' '), sub_spacer (indentation_ + 2, ' ');
 
-  buffer_ << spacer << "Handling exists higher block.\n";
+  unsigned int processes = builder_.program.processes.size ();
+  
+  bool started_i = false;
+  for (unsigned int i = 0; i + 1 < processes; ++i)
+  {
+    if (started_i)
+    {
+      buffer_ << " || \n" << sub_spacer;
+    }
+
+    buffer_ << "(";
+    buffer_ << "*id == " << i << " && (";
+
+    bool started_j = false;
+    for (unsigned int j = i + 1; j < processes; ++j)
+    {
+      if (started_j)
+        buffer_ << " || ";
+
+      id_map_ [expression.id] = j;
+      
+      visit (expression.arg);
+
+      id_map_.erase (expression.id);
+
+      if (!started_j)
+        started_j = true;
+    }
+
+    buffer_ << "))";
+
+    if (!started_i)
+      started_i = true;
+  }
 }
 
 
@@ -171,9 +299,42 @@ daig::madara::Function_Visitor::enterEXL (EXLExpr & expression)
 void
 daig::madara::Function_Visitor::exitEXL (EXLExpr & expression)
 {
-  std::string spacer (indentation_, ' ');
+  std::string spacer (indentation_, ' '), sub_spacer (indentation_ + 2, ' ');
 
-  buffer_ << spacer << "Handling exists lower block.\n";
+  unsigned int processes = builder_.program.processes.size ();
+  
+  bool started_i = false;
+  for (unsigned int i = 1; i < processes; ++i)
+  {
+    if (started_i)
+    {
+      buffer_ << " || \n" << sub_spacer;
+    }
+
+    buffer_ << "(";
+    buffer_ << "*id == " << i << " && (";
+
+    bool started_j = false;
+    for (unsigned int j = 0; j < i; ++j)
+    {
+      if (started_j)
+        buffer_ << " || ";
+
+      id_map_ [expression.id] = j;
+      
+      visit (expression.arg);
+
+      id_map_.erase (expression.id);
+
+      if (!started_j)
+        started_j = true;
+    }
+
+    buffer_ << "))";
+
+    if (!started_i)
+      started_i = true;
+  }
 }
 
 
@@ -411,7 +572,7 @@ daig::madara::Function_Visitor::exitBreak (BreakStmt &statement)
 {
   std::string spacer (indentation_, ' ');
 
-  buffer_ << spacer << "Handling break statement.\n";
+  buffer_ << spacer << "break;\n";
 }
 
 
@@ -427,7 +588,7 @@ daig::madara::Function_Visitor::exitCont (ContStmt &statement)
 {
   std::string spacer (indentation_, ' ');
 
-  buffer_ << spacer << "Handling continue statement.\n";
+  buffer_ << spacer << "continue;\n";
 }
 
 
@@ -443,7 +604,7 @@ daig::madara::Function_Visitor::exitRet (RetStmt &statement)
 {
   std::string spacer (indentation_, ' ');
 
-  buffer_ << spacer << "Handling return statement.\n";
+  buffer_ << spacer << "return result;\n";
 }
 
 
