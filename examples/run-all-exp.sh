76e58a1af1a28bd8f9a7c1499685bbd687461e38
Sagar Chaki <chaki@sei.cmu.edu>
2014-03-08 11:14:51 -0500

    added more rounds. code refactoring. increased time and memory limit of brunch since these will be controlled by run-exp.sh.

--- examples/run-all-exp.sh
+++ examples/run-all-exp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
-BRFMT="ExpName:Nodes:Rounds:CPLING-Time"
+BRFMT="ExpName:Nodes:Rounds:CPLING-Time:Status"
+BRUNCH="./brunch --cpu 100000 --mem 100000 --format $BRFMT"
 
 #coll-ok experiments
 function do_coll_ok {
@@ -10,12 +10,12 @@ function do_coll_ok {
         EXP=""
         for N in 2 4 6 8 10; do
             for X in 4 7 10; do
-                for R in 5 10 15 20; do
+                for R in 5 10 15 20 30 40 50; do
                     EXP="$EXP coll.ok.$X.$X:sync-coll-avoid.ok.dasl:$N:$R:--DX=$X--DY=$X"
                 done
             done
         done
-        ./brunch --out $OUTD --cpu 10000 --mem 5000 --format $BRFMT $EXP -- ./run-exp.sh
+        $BRUNCH --out $OUTD $EXP -- ./run-exp.sh
     else
         echo "directory $OUTD exists .. skipping"
     fi
@@ -29,13 +29,12 @@ function do_coll_bug1 {
         EXP=""
         for N in 2 4 6 8 10; do
             for X in 4 7 10; do
-                for R in 5 10 15 20; do
+                for R in 5 10 15 20 30 40 50; do
                     EXP="$EXP coll.bug1.$X.$X:sync-coll-avoid.bug1.dasl:$N:$R:--DX=$X--DY=$X"
                 done
             done
         done
-
-        ./brunch --out $OUTD --cpu 10000 --mem 5000 --format $BRFMT $EXP -- ./run-exp.sh
+        $BRUNCH --out $OUTD $EXP -- ./run-exp.sh
     else
         echo "directory $OUTD exists .. skipping"
     fi
@@ -48,13 +47,12 @@ function do_coll_bug2 {
         EXP=""
         for N in 2 4 6 8 10; do
             for X in 4 7 10; do
-                for R in 5 10 15 20; do
+                for R in 5 10 15 20 30 40 50; do
                     EXP="$EXP coll.bug2.$X.$X:sync-coll-avoid.bug2.dasl:$N:$R:--DX=$X--DY=$X"
                 done
             done
         done
-
-        ./brunch --out $OUTD --cpu 10000 --mem 5000 --format $BRFMT $EXP -- ./run-exp.sh
+        $BRUNCH --out $OUTD $EXP -- ./run-exp.sh
     else
         echo "directory $OUTD exists .. skipping"
     fi
