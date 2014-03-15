32e14aa1b6feeda3f637069206fd6c98b051cd44
Sagar Chaki <chaki@sei.cmu.edu>
2014-03-14 23:20:12 -0400

    new 3D collision avoidance algorithms and code to run experiments on it.

--- examples/run-all-exp.sh
+++ examples/run-all-exp.sh
@@ -60,6 +60,25 @@ function do_coll_3d {
 
 }
 
+#coll-3d-opt experiments
+function do_coll_3d_opt {
+    OUTD=brunch.out.coll.3d.opt.$1.$2
+    if [ ! -e $OUTD ]; then 
+        EXP=""
+        for N in 4 7 10; do
+            for X in 4 7 10; do
+                for R in 20 40 60 80 100; do
+                    EXP="$EXP coll.3d.opt.$1.$X.$X:sync-coll-3d-opt.$1.dasl:$N:$R:--DX=$X--DY=$X$2"
+                done
+            done
+        done
+        $BRUNCH --out $OUTD $EXP -- ./run-exp.sh
+    else
+        echo "directory $OUTD exists .. skipping"
+    fi
+
+}
+
 #mutex experiments
 function do_mutex {
     OUTD=brunch.out.mutex.$1.$2
