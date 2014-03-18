b88bf9832ec9942fccc87031d133484fd1952704
Sagar Chaki <chaki@sei.cmu.edu>
2014-03-18 19:32:25 -0400

    including results and script to generate tables in the distribution.

--- examples/mkdist.sh
+++ examples/mkdist.sh
@@ -15,6 +15,10 @@ run-exp.sh ../tools/daig/src/daslc/daslc brunch \
 /home/chaki/stuff/madara/madara/libMADARA.so.6.2.3 \
 /home/chaki/stuff/madara/ace/ACE_wrappers/ace/libACE.so.6.2.3 \
 sync-coll-3d-opt.*dasl sync-coll-avoid.*.dasl \
-sync-mutex.*.dasl $TMPD/$PN
+sync-mutex.*.dasl gen-tables.sh $TMPD/$PN
+mkdir $TMPD/$PN/results
+for i in coll coll.3d.opt mutex; do
+    cp -a results/brunch.out.${i}.ok* results/brunch.out.${i}.bug* $TMPD/$PN/results
+done
 (cd $TMPD; zip -r $PN.zip $PN)
 echo "package $TMPD/$PN.zip created"
