693be3d7511bd69efd408a09784981e8b2035987
Sagar Chaki <chaki@sei.cmu.edu>
2014-05-04 17:22:07 -0400

    simple example to test the VREP external API.

---
+++ simulation/vrep/testExtApi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+extern "C" {
+#include "extApi.h"
+#include "extApiPlatform.h"
+#include "extApiPlatform.h"
+}
+
+int main()
+{
+  std::cout << "hello world\n";
+  return 0;
+}
