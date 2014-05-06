a1c21d94bba0da89a56f1d893f05bf1f19895c89
Sagar Chaki <chaki@sei.cmu.edu>
2014-05-06 18:56:24 -0400

    some more work on the test example. checkpoint.

--- simulation/vrep/testExtApi.cpp
+++ simulation/vrep/testExtApi.cpp
@@ -1,13 +1,22 @@
+#include <stdlib.h>
 #include <unistd.h>
 #include <iostream>
 
 #include "DaslVrep.hpp"
 
-int main()
+int main(int argc,char **argv)
 {
+  if(argc == 1) {
+    std::cout << "ERROR: Usage: " << argv[0] << " port-number\n";
+    return 1;
+  }
+  
+  simxInt port = atoi(argv[1]);
+
   std::cout << "opening connection to VREP\n";
-  DaslVrep dv;
-  if(dv.connect() == -1) {
+  DaslVrep dv(10,10);
+
+  if(dv.connect((simxChar*)"127.0.0.1",port) == -1) {
     std::cerr << "ERROR: could not connect to VREP ...\n";
     return 1;
   }
@@ -17,43 +26,28 @@ int main()
   std::cout << "ping time is " << dv.getPingTime() << " ms\n";
 
   sleep(2);
-  simxInt model1 = dv.createNode();
-  std::cout << "created robot 1\n";
-  std::cout << "scene now has " << dv.getNumObjects() << " objects\n";
-  dv.moveNode(model1,0,0,0.5);
 
-  dv.destroyNode(model1);
+  if(port == 19900) {
+    simxInt node1 = dv.createNode();
+    std::cout << "created robot 1\n";
+    std::cout << "scene now has " << dv.getNumObjects() << " objects\n";
+    dv.placeNodeAt(node1,0,0,1);
 
-  // sleep(2);
+    dv.startSim(); sleep(5); dv.pauseSim(); sleep(5);
+    dv.startSim(); sleep(5); dv.stopSim();
 
-  // simxInt model2 = loadModel(clientId);
-  // std::cout << "created robot 2\n";
-  // std::cout << "scene now has " << getNumObjects(clientId) << " objects\n";
-  // moveModel(clientId,model2,.2,.2,0.0001);
+    //while(dv.moveNodeTo(node1,0,1,1)) sleep(1);
 
-  // sleep(2);
-  // simxStartSimulation(clientId,simx_opmode_oneshot_wait);
-  // std::cout << "simulation started ...\n";
+    dv.destroyNode(node1);
+  } else {
+    simxInt node1 = dv.createNode();
+    std::cout << "created robot 1\n";
+    std::cout << "scene now has " << dv.getNumObjects() << " objects\n";
+    dv.placeNodeAt(node1,1,0,1);
 
-  // sleep(5);
-  // simxPauseSimulation(clientId,simx_opmode_oneshot_wait);
-  // std::cout << "simulation paused ...\n";
-
-  // sleep(2);
-  // simxStartSimulation(clientId,simx_opmode_oneshot_wait);
-  // std::cout << "simulation started ...\n";
-
-  // sleep(5);
-  // simxStopSimulation(clientId,simx_opmode_oneshot_wait);
-  // std::cout << "simulation stopped ...\n";
-
-  // //sleep(5);
-  // deleteModel(clientId,model1);
-  // std::cout << "deleted robot1\n";
-
-  // //sleep(5);
-  // deleteModel(clientId,model2);
-  // std::cout << "deleted robot2\n";
+    sleep(2);
+    dv.destroyNode(node1);
+  }
 
   dv.disconnect();
   return 0;
