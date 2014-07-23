7721ccd3b75f256e2b2b9447d576a4757bfc5c06
Sagar Chaki <chaki@sei.cmu.edu>
2014-07-23 13:28:30 -0400

    checking number of inputs. other cosmetic changes.

--- examples/coll-avoid/coll-avoid-experiment.sh
+++ examples/coll-avoid/coll-avoid-experiment.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$#" != "4" ]; then
+    echo "Usage : $0 <num-nodes> <num-experiments> <num-runs> <out-file>"
+    echo "Example : $0 3 5 10 foo.out"
+    exit 1
+fi
+
 NUM_NODES=$1
 NUM_EXPERIMENTS=$2
 NUM_RUNS=$3
@@ -9,6 +15,6 @@ g++ -Wall coll-avoid-experiment.cpp -o coll-avoid-experiment
 
 for ((i=0; i < $NUM_EXPERIMENTS; i++))
 do
-	echo "Experiment $i:"
-	$MCDA_ROOT/examples/coll-avoid/coll-avoid-experiment $NUM_NODES $NUM_RUNS $OUT_FILE
+    echo "Experiment $i:"
+    $MCDA_ROOT/examples/coll-avoid/coll-avoid-experiment $NUM_NODES $NUM_RUNS $OUT_FILE
 done
