078d5108823b7391481bff33cb5c1d2e6d433a16
Sagar Chaki <chaki@sei.cmu.edu>
2014-07-23 21:49:09 -0400

    do multiple runs in parallel. use different MADARA domains to prevent runs from interfering with each other.

--- examples/coll-avoid/coll-avoid-experiment.sh
+++ examples/coll-avoid/coll-avoid-experiment.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-if [ "$#" != "4" ]; then
-    echo "Usage : $0 <num-nodes> <num-experiments> <num-runs> <out-file>"
-    echo "Example : $0 3 5 10 foo.out"
+if [ "$#" != "3" ]; then
+    echo "Usage : $0 <num-nodes> <num-experiments> <out-file>"
+    echo "Example : $0 3 10 foo.out"
     exit 1
 fi
 
 NUM_NODES=$1
 NUM_EXPERIMENTS=$2
-NUM_RUNS=$3
-OUT_FILE=$4
+OUT_FILE=$3
 
 g++ -Wall coll-avoid-experiment.cpp -o coll-avoid-experiment
 
+#names of output directories
+TMPDS=""
+
 for ((i=0; i < $NUM_EXPERIMENTS; i++))
 do
-    echo "Experiment $i:"
-    $MCDA_ROOT/examples/coll-avoid/coll-avoid-experiment $NUM_NODES $NUM_RUNS $OUT_FILE
+    TMPD=$(mktemp -d /tmp/coll-avoid.XXXXXXXX)
+    echo "Experiment $i: $TMPD"
+    TMPDS="$TMPDS $TMPD"
+    echo "Experiment $i:" > $TMPD/out
+    $MCDA_ROOT/examples/coll-avoid/coll-avoid-experiment $NUM_NODES dom-$i $TMPD 2>&1 >> $TMPD/out &
+    sleep 1
+done
+
+#wait for experiments to finish, then concatenate outputs
+wait
+rm -f $OUT_FILE
+echo 'Coll  AvgSpeed  Timeout' > $OUT_FILE
+for i in $TMPDS; do
+    cat $i/out
+    cat $i/stats >> $OUT_FILE
 done
