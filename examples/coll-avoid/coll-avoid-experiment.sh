73630341b882e67fce5d79df451a2fe7f9853004
Sagar Chaki <chaki@sei.cmu.edu>
2014-07-23 23:06:46 -0400

    added option to select between disc and conservative methods.

--- examples/coll-avoid/coll-avoid-experiment.sh
+++ examples/coll-avoid/coll-avoid-experiment.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-if [ "$#" != "3" ]; then
-    echo "Usage : $0 <num-nodes> <num-experiments> <out-file>"
-    echo "Example : $0 3 10 foo.out"
+if [ "$#" != "4" ]; then
+    echo "Usage : $0 <num-nodes> <num-experiments> <use-disc> <out-file>"
+    echo "Example : $0 3 10 1 foo.out (to use disc method)"
+    echo "Example : $0 3 10 0 foo.out (to use conservative method)"
     exit 1
 fi
 
 NUM_NODES=$1
 NUM_EXPERIMENTS=$2
-OUT_FILE=$3
+USE_DISC=$3
+OUT_FILE=$4
 
 g++ -Wall coll-avoid-experiment.cpp -o coll-avoid-experiment
-daslc --nodes $NUM_NODES --madara --out coll-avoid.cpp coll-avoid.dasl
+daslc --DUSE_DISC_METHOD $USE_DISC --nodes $NUM_NODES --madara --out coll-avoid.cpp coll-avoid.dasl
 g++ -I$ACE_ROOT -I$MADARA_ROOT/include -o coll-avoid coll-avoid.cpp \
 $MADARA_ROOT/libMADARA.so $ACE_ROOT/lib/libACE.so
 
