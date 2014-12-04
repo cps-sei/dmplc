308bb763845be20b25d7c7232d8715c1bd253870
Sagar Chaki <chaki@sei.cmu.edu>
2014-12-04 11:47:03 -0500

    -- deleted examples folder. -- created new folder test with subfolders for david and sagar. -- moved dart* examples from docs/tutorial to test/david. -- made changes to Makefiles and DMPL files so that everything compiles. -- delete some superfluous files.

--- dmpl/examples/coll-avoid/compile-dasl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dmplc --nodes $2 --madara --out $1.cpp $1.dmpl
-g++ -I$ACE_ROOT -I$MADARA_ROOT/include -o $1 $1.cpp $MADARA_ROOT/libMADARA.so $ACE_ROOT/lib/libACE.so
-
--- dmpl/examples/madara/sync/start_sync_coll_sim.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCENE_DIR=`pwd`
-cd $VREP_HOME
-sh ./vrep.sh $SCENE_DIR/sync-coll-avoid.ttt
