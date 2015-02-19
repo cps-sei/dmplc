c33d9036daccc127a037b1113c89cc5736b280d3
Sagar Chaki <chaki@sei.cmu.edu>
2015-02-18 22:00:17 -0500

    added parameters and return values for the adaptation manager.

--- dmpl/docs/tutorial/example-05/dmpl/adaptation_manager.h
+++ dmpl/docs/tutorial/example-05/dmpl/adaptation_manager.h
@@ -4,7 +4,29 @@
 
 //-- an empty adaptation manager stub. this will be ultimately
 //-- replaced by Gabe's implementation.
-void adaptation_manager()
+int adaptation_manager
+(
+ int current_formation,
+ int changing, //-- whether in process of changing to other formation
+ double change_progress, //-- progress in changing to other formation
+ //-- next 2 hazards
+ double hazard1,
+ double hazard2,    
+ double hazard3,    
+ double hazard4,    
+ double hazard5,    
+ //-- next 2 coverages
+ double cover1,
+ double cover2,
+ double cover3,
+ double cover4,
+ double cover5,
+ double loose_speed, //-- speed of loose formation
+ double tight_speed, //-- speed of tight formation
+ double loose_power, //-- power needed in loose formation
+ double tight_power, //-- power needed in tight formation
+ double energy //-- amount of energy left
+)
 {
 }
 
