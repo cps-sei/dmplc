43dba0423fde89004854e7f5237a0d973c421cbe
Sagar Chaki <chaki@sei.cmu.edu>
2015-02-18 20:48:31 -0500

    added task for adaptation manager.

---
+++ dmpl/docs/tutorial/example-05/dmpl/adaptation_manager.h
@@ -0,0 +1,13 @@
+/**
+ * Interface with the adaptation manager.
+ */
+
+//-- an empty adaptation manager stub. this will be ultimately
+//-- replaced by Gabe's implementation.
+void adaptation_manager()
+{
+}
+
+/*********************************************************************/
+//end of file
+/*********************************************************************/
