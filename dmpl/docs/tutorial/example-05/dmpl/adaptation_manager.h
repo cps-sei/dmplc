bda839d4e7401611bf54c84c077919f3ea0029b7
Sagar Chaki <chaki@sei.cmu.edu>
2015-03-05 12:31:44 -0500

    updated to new adaptation manager API supplied by Gabe.

--- dmpl/docs/tutorial/example-05/dmpl/adaptation_manager.h
+++ dmpl/docs/tutorial/example-05/dmpl/adaptation_manager.h
@@ -6,26 +6,26 @@
 //-- replaced by Gabe's implementation.
 int adaptation_manager
 (
- int current_formation,
+ int current_formation, //-- 0 loose, 1 tight
  int changing, //-- whether in process of changing to other formation
- double change_progress, //-- progress in changing to other formation
- //-- next 2 hazards
+ double change_progress, //-- progress in changing to other formation (remaining time)
+ //-- next n hazards (for the next n segments in the path)
  double hazard1,
- double hazard2,    
- double hazard3,    
- double hazard4,    
- double hazard5,    
- //-- next 2 coverages
- double cover1,
- double cover2,
- double cover3,
- double cover4,
- double cover5,
+ double hazard2,
+ double hazard3,
+ double hazard4,
+ double hazard5,
+ //-- variance of the next n hazards predictions
+ double variance1,
+ double variance2,
+ double variance3,
+ double variance4,
+ double variance5,
  double loose_speed, //-- speed of loose formation
  double tight_speed, //-- speed of tight formation
- double loose_power, //-- power needed in loose formation
- double tight_power, //-- power needed in tight formation
- double energy //-- amount of energy left
+ double segment_length, //-- length of a segment in the path
+ int remaining_segments, //-- number of segments remaining in the path
+ double remaining_flight_time //-- how much flight time the formation has left
 )
 {
 }
