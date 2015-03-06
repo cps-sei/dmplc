ba41b178eb06ee3211b132c5641bc47e2481a226
Sagar Chaki <chaki@sei.cmu.edu>
2015-03-06 17:10:46 -0500

    increased probability of formation change to 0.25. also, don't change formation if another change is ongoing.

--- dmpl/docs/tutorial/example-05/dmpl/adaptation_manager.h
+++ dmpl/docs/tutorial/example-05/dmpl/adaptation_manager.h
@@ -31,7 +31,7 @@ int adaptation_manager
  double remaining_flight_time //-- how much flight time the formation has left
 )
 {
-  if(random() % 20 == 0) return 1 - current_formation;
+  if(!changing && (random() % 4 == 0)) return 1 - current_formation;
   else return current_formation;
 }
 
