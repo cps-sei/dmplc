a0d7dd6dbcac6f24b30ba1450f41d4a67de9f85e
Sagar Chaki <chaki@sei.cmu.edu>
2015-05-29 15:11:27 -0400

    deleted unnecessary files.

--- simulation/generate_vs2010.bat
+++
@@ -1,2 +0,0 @@
-mwc.pl -type vc10 CollisionSimulation.mwc
-"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" ".\CollisionSimulation.sln"
--- simulation/run_sim.bat
+++
@@ -1,2 +0,0 @@
-start cmd /k "bin\SimpleSim.exe 0"
-start cmd /k "bin\SimpleSim.exe 1"
