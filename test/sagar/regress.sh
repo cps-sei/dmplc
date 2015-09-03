e3d7c1661961496f1557237037cc579a46e96d36
Sagar Chaki <chaki@sei.cmu.edu>
2015-09-02 23:17:44 -0400

    renamed test case files so they don't clash with the ones used in the tutorials.

--- test/sagar/regress.sh
+++ test/sagar/regress.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-printf "testing %35s : " "example-01.dmpl"
-rm -f example-01.cpp
+printf "testing %35s : " "test-example-01.dmpl"
+rm -f test-example-01.cpp
 dmplc -n 3 --DX 10 --DY 10 --DTopY 2.25 --DBottomY -2.25 --DLeftX -2.25 \
-      --DRightX 2.25 -g -o example-01.cpp example-01.dmpl &> /dev/null
-diff example-01.cpp example-01.cpp.saved &> /dev/null
+      --DRightX 2.25 -g -o test-example-01.cpp test-example-01.dmpl &> /dev/null
+diff test-example-01.cpp test-example-01.cpp.saved &> /dev/null
 if [ "$?" == "0" ]; then echo "SUCCESS"; else echo "FAILURE"; fi
 
 ##parse a file and output. then parse this output and output
@@ -22,6 +22,6 @@ function test_double_parse {
     if [ "$?" == "0" ]; then echo "SUCCESS"; else echo "FAILURE"; fi
 }
 
-test_double_parse example-01.dmpl
+test_double_parse test-example-01.dmpl
 test_double_parse test-01.dmpl
-test_double_parse example-05.dmpl
+test_double_parse test-example-05.dmpl
