4755149f6ab1626b04e3cdcec63a3b59eae3c535
Sagar Chaki <chaki@sei.cmu.edu>
2015-11-04 08:02:21 -0500

    added comments to options. removed unusued options. removed commented out code.

--- src/dmplc/dmplc.cpp
+++ src/dmplc/dmplc.cpp
@@ -69,22 +69,51 @@
 /*********************************************************************/
 //options
 /*********************************************************************/
+
+//-- names of files to parse
 std::list<std::string> file_names;
+
+//-- output file name. empty means stdout.
 std::string out_file;
+
+//-- name of target thunk to dump from the DMPL file.
 std::string madara_target ("GNU_CPP");
-bool do_print = false, do_seq = false, do_seq_ind = false, do_gams = false;
-bool do_vrep = false, do_expect = false, do_analyzer = false;
-bool debug = false;
-bool seq_no_array = false, init_globals = false;
-bool statistical = false;
-double stat_hertz = 10;
-std::string reqProp; //-- name of the require property to verify
+
+//-- number of nodes in system (to generate code for, verify etc.)
 size_t nodes = 0;
+
+//-- parse and print the DMPL file
+bool do_print = false;
+
+//-- sequentialize for verification via bounded model checking
+bool do_seq = false;
+
+//-- sequential for checking inductive invariants for bounded model
+//-- checking
+bool do_seq_ind = false;
+
+//-- number of rounds to sequentialize for (-1 means infinity)
 int round_num = -1;
 
+//-- generate code against the GAMS platform
+bool do_gams = false;
+
+//-- generate code to check and log "expect" statements
+bool do_expect = false;
+
+//-- generate "expect" log analyzer
+bool do_analyzer = false;
+
+//-- print debug messages
+bool debug = false;
+
+//-- name of the require property to verify
+std::string reqProp; 
+
+//-- the type of thread scheduler to use
 dmpl::SchedType schedType = dmpl::NON_RT;
 
-//constant definitions supplied via command line
+//-- constant definitions supplied via command line
 std::map<std::string, std::string> const_def;
 
 /*********************************************************************/
@@ -131,7 +160,8 @@ int main (int argc, char **argv)
       program.processes.push_back (dmpl::Process (nodeName, i));
 
     //create a madara builder instance of the dmpl builder parse
-    dmpl::CodeGenerator *gams_builder = new dmpl::gams::GAMS_Builder (builder, madara_target, schedType, do_expect);
+    dmpl::CodeGenerator *gams_builder
+      = new dmpl::gams::GAMS_Builder (builder, madara_target, schedType, do_expect);
 
     //build the generated code
     gams_builder->build ();
@@ -167,8 +197,7 @@ int main (int argc, char **argv)
     //print the generated code
     if (out_file.empty ())
       ana_builder->print (std::cout);
-    else
-    {
+    else {
       std::ofstream os (out_file.c_str ());
       ana_builder->print (os);
       os.close ();
@@ -195,21 +224,9 @@ int main (int argc, char **argv)
     syncSeqDbl.run ();
     cprog = syncSeqDbl.cprog;
     
-    //eliminate arrays
-    /*
-    if (seq_no_array) {
-      dmpl::ArrayElim ae (cprog, init_globals);
-      ae.run ();
-      cprog = ae.outProg;
-    }
-    */
-    
     if (out_file.empty ())
-      {
-        cprog.print (std::cout, 0);
-      }
-    else
-      {
+      cprog.print (std::cout, 0);
+    else {
         std::ofstream os (out_file.c_str ());
         cprog.print (os, 0);
         os.close ();
@@ -233,19 +250,9 @@ int main (int argc, char **argv)
     syncSeqDblInd.run ();
     cprog = syncSeqDblInd.cprog;
     
-    //eliminate arrays
-    /*if (seq_no_array) {
-      dmpl::ArrayElim ae (cprog, init_globals);
-      ae.run ();
-      cprog = ae.outProg;
-    }*/
-    
     if (out_file.empty ())
-      {
-        cprog.print (std::cout, 0);
-      }
-    else
-      {
+      cprog.print (std::cout, 0);
+    else {
         std::ofstream os (out_file.c_str ());
         cprog.print (os, 0);
         os.close ();
@@ -340,29 +347,6 @@ void parse_options (int argc, char **argv)
       }
       ++i;
     }
-    else if (arg1 == "-vr" || arg1 == "--vrep")
-    {
-      do_vrep = true;
-    }
-    else if (arg1 == "-st" || arg1 == "--statistic")
-    {
-      statistical = true;
-    }
-    else if (arg1 == "-sh" || arg1 == "--stat-hertz")
-    {
-      if (i + 1 < argc)
-      {
-        std::stringstream buffer (argv[i + 1]);
-        buffer >> stat_hertz;
-      }
-      else
-      {
-        std::cerr << "ERROR: Stat rate (-sh|--stat-hertz) must have a ";
-        std::cerr << " number of hertz (e.g. -sh 10)\n";
-        usage (argv[0]);
-      }
-      ++i;
-    }
 #if MZSRM==1
     else if (arg1 == "-mz" || arg1 == "--mzsrm")
     {
@@ -391,14 +375,6 @@ void parse_options (int argc, char **argv)
         usage (argv[0]);
       }
       ++i;
-    }
-    /*else if (arg1 == "--seq-no-array")
-    {
-      seq_no_array = true;
-    }*/
-    else if (arg1 == "-i" || arg1 == "--init-globals")
-    {
-      init_globals = true;
     }
     else if (arg1.substr (0,3) == "--D")
     {
@@ -444,23 +420,18 @@ void usage (char *cmd)
   std::cerr << "  -o|--out file            output file, default is stdout\n";
   std::cerr << "  -p|--print               parse and print DASL file\n";
   std::cerr << "  -n|--nodes nodes         number of nodes\n";
-  std::cerr << "  -rp|--reqProp prop_name  name of require property to verify\n";
+  std::cerr << "  -rp|--reqProp name       name of require property to verify\n";
   std::cerr << "  -a|--analyzer            generate C++ for expect log analyzer\n";
   std::cerr << "  -g|--gams                generate C++/GAMS code to run\n";
   std::cerr << "  -e|--expect              check and log 'expect' statements\n";
   std::cerr << "  -t|--target|--platform p specify a target platform\n";
   std::cerr << "                           Available platforms: WIN_CPP, GNU_CPP (default)\n";
-  std::cerr << "  -vr|--vrep               generate code that targets VREP\n";
-  std::cerr << "  -st|--statistic          include statistical model checking support\n";
-  std::cerr << "  -sh|--stat-hertz [hertz] gathered stats at hertz rate (default " << stat_hertz << ")\n";
 #if MZSRM==1
   std::cerr << "  -mz|--mzsrm              generate code that targets MZSRM scheduler\n";
 #endif
   std::cerr << "  -s|--seq                 generate sequentialized code to verify\n";
   std::cerr << "  -si|--seq-ind            generate sequentialized code to verify inductiveness\n";
   std::cerr << "  -r|--rounds rounds       number of verification rounds\n";
-  //std::cerr << "  --seq-no-array           do not use arrays during verification\n";
-  std::cerr << "  -i|--init-globals        initialize global variables\n";
   std::cerr << "  --D<const_name> value    set a const to a value\n";
   exit (0);
 }
