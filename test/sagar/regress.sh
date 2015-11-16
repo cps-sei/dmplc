dd6daae05074240445d5ad6621a11c89a8af3b13
Sagar Chaki <chaki@sei.cmu.edu>
2015-11-15 22:22:47 -0500

    update regression script to check for correct result of sequentialization.

--- test/sagar/regress.sh
+++ test/sagar/regress.sh
@@ -9,7 +9,21 @@ function test_code_gen {
     OUT1="$BN.cpp"
     OUT2="$BN.cpp.saved"
     rm -f $OUT1
-    dmplc --roles $2 --DX 10 --DY 10 --DTopY 2.25 --DBottomY -2.25 --DLeftX -2.25 --DRightX 2.25 -g -o $OUT1 $DMPL &> /dev/null
+    dmplc --roles $ROLES --DX 10 --DY 10 --DTopY 2.25 --DBottomY -2.25 --DLeftX -2.25 --DRightX 2.25 -g -o $OUT1 $DMPL &> /dev/null
+    diff $OUT1 $OUT2 &> /dev/null
+    if [ "$?" == "0" ]; then echo "SUCCESS"; else echo "FAILURE"; fi
+}
+
+##sequentialize and check against correct output
+function test_seq {
+    DMPL="$1"
+    ROLES="$2"
+    printf "sequentialize%30s : " "$DMPL"
+    BN=$(basename $DMPL .dmpl)
+    OUT1="$BN.c"
+    OUT2="$BN.c.saved"
+    rm -f $OUT1
+    dmplc --roles $ROLES -r 5 -s -rp NoCollision -o $OUT1 $DMPL &> /dev/null
     diff $OUT1 $OUT2 &> /dev/null
     if [ "$?" == "0" ]; then echo "SUCCESS"; else echo "FAILURE"; fi
 }
@@ -24,8 +38,8 @@ function test_double_parse {
     OUT1="$BN.1.dmpl"
     OUT2="$BN.2.dmpl"
     rm -f $OUT1 $OUT2
-    dmplc --roles $2 --DX 10 --DY 10 --DTopY 2.25 --DBottomY -2.25 --DLeftX -2.25 --DRightX 2.25 -p -o $OUT1 $DMPL &> /dev/null
-    dmplc --roles $2 --DX 10 --DY 10 --DTopY 2.25 --DBottomY -2.25 --DLeftX -2.25 --DRightX 2.25 -p -o $OUT2 $OUT1 &> /dev/null
+    dmplc --roles $ROLES --DX 10 --DY 10 --DTopY 2.25 --DBottomY -2.25 --DLeftX -2.25 --DRightX 2.25 -p -o $OUT1 $DMPL &> /dev/null
+    dmplc --roles $ROLES --DX 10 --DY 10 --DTopY 2.25 --DBottomY -2.25 --DLeftX -2.25 --DRightX 2.25 -p -o $OUT2 $OUT1 &> /dev/null
     diff $OUT1 $OUT2 &> /dev/null
     if [ "$?" == "0" ]; then echo "SUCCESS"; else echo "FAILURE"; fi
 }
@@ -36,3 +50,4 @@ test_double_parse test-example-01c.dmpl uav:Uav1:2:uav:Uav2:1
 test_double_parse test-01.dmpl uav:Leader:1:uav:Protector:4
 test_double_parse test-example-05.dmpl uav:Leader:1:uav:Protector:4
 test_code_gen test-example-01a.dmpl uav:Uav:3
+test_seq test-example-01c.dmpl uav:Uav1:2:uav:Uav2:1
