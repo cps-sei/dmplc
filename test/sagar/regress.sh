a59edc717dec6fcd0c4e3f364d390c6215f1c4eb
Sagar Chaki <chaki@sei.cmu.edu>
2015-11-15 22:17:04 -0500

    deleted commented out code.

--- test/sagar/regress.sh
+++ test/sagar/regress.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# printf "testing %35s : " "test-example-01.dmpl"
-# rm -f test-example-01.cpp
-# dmplc -n 3 --DX 10 --DY 10 --DTopY 2.25 --DBottomY -2.25 --DLeftX -2.25 \
-#       --DRightX 2.25 -g -o test-example-01.cpp test-example-01.dmpl &> /dev/null
-# diff test-example-01.cpp test-example-01.cpp.saved &> /dev/null
-# if [ "$?" == "0" ]; then echo "SUCCESS"; else echo "FAILURE"; fi
-
 ##generate code and check against correct output
 function test_code_gen {
     DMPL="$1"
