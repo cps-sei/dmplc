98b9be3a3d155f6daa2a102cad393411ddcfcc04
Sagar Chaki <chaki@sei.cmu.edu>
2015-11-22 11:39:18 -0500

    bug fix in check for functions that must be skipped when generating code.

--- src/dmpl/gams/GAMS_Builder.cpp
+++ src/dmpl/gams/GAMS_Builder.cpp
@@ -78,9 +78,7 @@ namespace
   /*******************************************************************/
   bool skip_func(const dmpl::Func & function)
   {
-    return (function->attrs.count("InitSim") == 0 && (function->isExtern ||
-      function->attrs.count("INIT") > 0 || function->attrs.count("SAFETY") > 0 ||
-      !function->usage_summary.anyNonExpect().any()));
+    return (function->attrs.count("InitSim") == 0 && function->isExtern);
   }
   
   /*******************************************************************/
