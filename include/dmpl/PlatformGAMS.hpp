f148a9f63e2c9a03247578450d62220acaf80075
Sagar Chaki <chaki@sei.cmu.edu>
2015-11-28 19:58:37 -0500

    added a third dimension to the GAMS platform.

--- include/dmpl/PlatformGAMS.hpp
+++ include/dmpl/PlatformGAMS.hpp
@@ -60,10 +60,11 @@
 //-- GAMS variables
 /********************************************************************/
 gams::platforms::Base_Platform *platform = NULL;
-int grid_x = 0, grid_y = 0;
+int grid_x = 0, grid_y = 0, grid_z = 0;
 double grid_leftX = NAN, grid_rightX = NAN;
 double grid_topY = NAN, grid_bottomY = NAN;
-double grid_cellX = NAN, grid_cellY = NAN;
+double grid_topZ = NAN, grid_bottomZ = NAN;
+double grid_cellX = NAN, grid_cellY = NAN, grid_cellZ = NAN;
 
 /********************************************************************/
 //-- GAMS functions
@@ -80,16 +81,16 @@ void GRID_INIT(int x, int y, double leftX, double rightX, double topY, double bo
   grid_cellY = (grid_bottomY - grid_topY) / (grid_y-1);
 }
 
-void GRID_PLACE(int x, int y, double alt = 0.0)
+void GRID_PLACE(int x, int y, int z)
 {
   knowledge.set(".initial_x", grid_leftX + x * grid_cellX);
   knowledge.set(".initial_y", grid_topY + y * grid_cellY);
-  knowledge.set(".initial_alt", alt);
+  knowledge.set(".initial_alt", grid_topZ + z * grid_cellZ);
 }
 
-int GRID_MOVE(int x, int y, double alt = NAN, double epsilon = 0.1)
+int GRID_MOVE(int x, int y, int z, double epsilon = 0.1)
 {
-  int ret = platform->move(gams::utility::Position(grid_leftX + x * grid_cellX, grid_topY + y * grid_cellY, alt), epsilon);
+  int ret = platform->move(gams::utility::Position(grid_leftX + x * grid_cellX, grid_topY + y * grid_cellY, grid_topZ + z * grid_cellZ), epsilon);
   return ret != 2;
 }
 
@@ -99,6 +100,11 @@ double GET_X()
 }
 
 double GET_Y()
+{
+  return 0;
+}
+
+double GET_Z()
 {
   return 0;
 }
