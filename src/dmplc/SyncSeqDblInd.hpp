53a8a0ff57ca2716b2d1ea4f30b1b024f976ed17
Sagar Chaki <chaki@sei.cmu.edu>
2015-12-02 09:17:32 -0500

    merging SyncSeqDbl and SyncSeqDblInd. checkpoint. work in progress.

--- src/dmplc/SyncSeqDblInd.hpp
+++ src/dmplc/SyncSeqDblInd.hpp
@@ -63,10 +63,7 @@
 #ifndef __SYNC_SEQ_DBL_IND_HPP__
 #define __SYNC_SEQ_DBL_IND_HPP__
 
-#include <iostream>
-#include "DmplBuilder.hpp"
-#include "dmpl/CProgram.h"
-#include "CopyVisitor.hpp"
+#include "SyncSeqDbl.hpp"
 
 namespace dmpl {
 
@@ -79,76 +76,20 @@ namespace dmpl {
     /*****************************************************************/
     //a visitor that transforms at the global level
     /*****************************************************************/
-    struct GlobalTransformer : public CopyVisitor
+    struct GlobalTransformer : public syncseqdbl::GlobalTransformer
     {
-      ///reference to callee class for callbacks
-      SyncSeqDblInd &syncSeq;
-
-      //the DASL program being transformed
-      dmpl::Program &prog;
-
-      //the function being processed, if any
-      Func func;
-
-      //map from variables to constants for substitution
-      std::map<std::string,size_t> idMap;
-
       //constructors
-      GlobalTransformer(SyncSeqDblInd &ss,dmpl::Program &p,const Func &fn) 
-        : syncSeq(ss),prog(p),func(fn) {}
-
-      //update substitution mapping
-      void addIdMap(const std::string &s,size_t i);
-      void delIdMap(const std::string &s);
-
-      //dispatchers
-      void exitLval(LvalExpr &expr);
-      void exitComp(CompExpr &expr);
-      void exitPrivate(PrivateStmt &stmt);
-      void exitCall(CallStmt &stmt);
-      bool enterFAN(FANStmt &stmt) { return false; }
-      void exitFAN(FANStmt &stmt);
-      bool enterFADNP(FADNPStmt &stmt) { return false; }
-      void exitFADNP(FADNPStmt &stmt);
-
-      std::string getNodeStr(const LvalExpr &expr) const;
+      GlobalTransformer(SyncSeqDbl &ss,dmpl::Program &p,const Func &fn)
+        : syncseqdbl::GlobalTransformer(ss,p,true,fn) {}
     };
 
     /*****************************************************************/
     //a visitor that transforms at the node level
     /*****************************************************************/
-    struct NodeTransformer : public GlobalTransformer
+    struct NodeTransformer : public syncseqdbl::NodeTransformer
     {
-      ///the process being transformed
-      Process proc;
-
-      //flags to indicate whether we are processing a function call, or
-      //lhs of an assignment
-      bool inCall, inLhs;
-
-      //direction -- true if forward, false if backward
-      bool fwd;
-
-      NodeTransformer(SyncSeqDblInd &ss,Program &p,const Process &pr,bool f,const Func &fn)
-        : GlobalTransformer(ss,p,fn),proc(pr),inCall(0),inLhs(0),fwd(f) {}
-
-      void exitLval(LvalExpr &expr);
-      bool enterCall(CallExpr &expr) { return false; }
-      void exitCall(CallExpr &expr);
-      bool enterEXO(EXOExpr &expr) { return false; }
-      void exitEXO(EXOExpr &expr);
-      bool enterEXH(EXHExpr &expr) { return false; }
-      void exitEXH(EXHExpr &expr);
-      bool enterEXL(EXLExpr &expr) { return false; }
-      void exitEXL(EXLExpr &expr);
-      bool enterAsgn(AsgnStmt &stmt) { return false; }
-      void exitAsgn(AsgnStmt &stmt);
-      bool enterFAO(FAOStmt &stmt) { return false; }
-      void exitFAO(FAOStmt &stmt);
-      bool enterFAOL(FAOLStmt &stmt) { return false; }
-      void exitFAOL(FAOLStmt &stmt);
-      bool enterFAOH(FAOHStmt &stmt) { return false; }
-      void exitFAOH(FAOHStmt &stmt);
+      NodeTransformer(SyncSeqDbl &ss,Program &p,const Process &pr,bool f,const Func &fn)
+        : syncseqdbl::NodeTransformer(ss,p,pr,f,fn) {}
     };
 
   } //namespace syncseqdblind
@@ -156,49 +97,14 @@ namespace dmpl {
   /*******************************************************************/
   //sequentializer for synchronous
   /*******************************************************************/
-  class SyncSeqDblInd
+  class SyncSeqDblInd : public SyncSeqDbl
   {
   public:
-    DmplBuilder &builder;  //-- the builder containing parsed DMPL file
-    std::string property;  //-- the name of target require property
-    int roundNum;          //-- the number of rounds
-    CProgram cprog;        //-- the generated C program
-
-    //-- map from processes to local and global variables that are
-    //-- relevant to the target property
-    std::map<Process,VarSet> relevantGlobs;
-    std::map<Process,VarSet> relevantLocs;
-
-    //-- map from processes to threads that are relevant to the target
-    //-- property
-    std::map<Process,Func> relevantThreads;
-
-    //-- map from processes to functions that are relevant to the
-    //-- target property
-    std::map<Process,std::set<Func>> relevantFuncs;
-
-    //-- map from processes to local and global variables that must be
-    //-- havoced, i.e., variables written by other threads and
-    //-- accessed by this process
-    std::map<Process,VarSet> havocGlobs;
-    std::map<Process,VarSet> havocLocs;
-    
-    //-- set of variables for which non-det functions have been created
-    std::set<std::string> nondetFuncs;
-
     SyncSeqDblInd(DmplBuilder &b, const std::string &p, int r);
 
-    //-- compute relevant variables and functions for each process
-    void computeRelevant();
-    
-    bool isRelevantVar(const Process &proc,const Expr &expr);
-    void createGlobVars();
-    void createCopyStmts(bool fwd,const Var &var,StmtList &res,ExprList indx,int node);
-    void createRoundCopier();
     void callFunction(const std::string &funcName,StmtList &body);
     void callRoundFuncs(StmtList &body);
     void createMainFunc();
-    Stmt createInitVar(const Var &var, const Process &proc);
     void createInit();
     void createSafety();
     void createAssume();
