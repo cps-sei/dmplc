660496eba20bef5a5cef31e2dcf1e1b566a26fd0
Sagar Chaki <chaki@sei.cmu.edu>
2016-03-23 16:57:04 -0400

    when generating code for initializing temporary variables of a function, use the appropriate thread in whose context the function is to be executed.

--- src/dmpl/gams/GAMSBuilder.cpp
+++ src/dmpl/gams/GAMSBuilder.cpp
@@ -153,12 +153,13 @@ namespace
   //-- print statements initializing temporary variables of a function
   /*******************************************************************/
   void print_temp_inits(const dmpl::Func &func, const dmpl::Node &node,
-                        dmpl::DmplBuilder & builder_, std::stringstream & buffer_)
+                        const dmpl::Func &thread, dmpl::DmplBuilder & builder_,
+                        std::stringstream & buffer_)
   {
     for(const dmpl::Var & variable : func->temps) {
       if(variable->initFunc == NULL) continue;
       
-      dmpl::madara::GAMSCompiler visitor (variable->initFunc, node, dmpl::Func(),
+      dmpl::madara::GAMSCompiler visitor (variable->initFunc, node, thread,
                                           builder_, buffer_, false, false);
       for (const dmpl::Stmt & statement : variable->initFunc->body)
         visitor.visit (statement);
@@ -1254,7 +1255,7 @@ dmpl::gams::GAMSBuilder::build_nodes (void)
           buffer_ << "void initialize_" << rec->name << " ()\n{\n";
           buffer_ << "  engine::Variables vars;\n";
           print_vars(buffer_, rec->initFunc->temps, false);
-          print_temp_inits(rec->initFunc, n->second, builder_, buffer_); 
+          print_temp_inits(rec->initFunc, n->second, Func(), builder_, buffer_); 
           
           //-- transform statements
           dmpl::madara::GAMSCompiler visitor (rec->initFunc, n->second, Func(),
@@ -1267,7 +1268,7 @@ dmpl::gams::GAMSBuilder::build_nodes (void)
         if(rec->assumeFunc != NULL && !rec->assumeFunc->body.empty()) {
           buffer_ << "int check_init_" << rec->name << " ()\n{\n";
           print_vars(buffer_, rec->assumeFunc->temps, false);
-          print_temp_inits(rec->assumeFunc, n->second, builder_, buffer_); 
+          print_temp_inits(rec->assumeFunc, n->second, Func(), builder_, buffer_); 
         
           //-- transform statements
           dmpl::madara::GAMSCompiler visitor (rec->assumeFunc, n->second, Func(),
@@ -1341,7 +1342,7 @@ dmpl::gams::GAMSBuilder::build_constructor_for_variable (Var &v, Node &node)
   //-- generate constructor if one was defined
   if(v->initFunc != NULL) {
     print_vars(buffer_, v->initFunc->temps, false);
-    print_temp_inits(v->initFunc, node, builder_, buffer_); 
+    print_temp_inits(v->initFunc, node, Func(), builder_, buffer_); 
     
     //-- transform statements
     dmpl::madara::GAMSCompiler visitor (v->initFunc, node, Func(),
@@ -1565,7 +1566,7 @@ dmpl::gams::GAMSBuilder::build_function (
   buffer_ << "\n  //-- Declare local (parameter and temporary) variables\n";
   print_vars(buffer_, actualFunc->params, true);
   print_vars(buffer_, actualFunc->temps, false);
-  print_temp_inits(actualFunc, node, builder_, buffer_); 
+  print_temp_inits(actualFunc, node, thread, builder_, buffer_); 
   buffer_ << "\n";
 
   buffer_ << "\n  //-- Begin function body\n";
