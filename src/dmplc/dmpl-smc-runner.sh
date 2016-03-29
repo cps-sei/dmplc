e14935910fcde758adcd7c5b0a564eb0ccaf8c8b
Sagar Chaki <chaki@sei.cmu.edu>
2016-03-28 20:42:14 -0400

    put some debugging messages.

--- src/dmplc/dmpl-smc-runner.sh
+++ src/dmplc/dmpl-smc-runner.sh
@@ -60,6 +60,12 @@ if [ -f /tmp/dart-run.sh.$PPID ]; then
     sleep 30
 fi
 
+echo ">>> hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+echo ">>> hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+echo ">>> hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+echo ">>> hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+echo ">>> hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+
 echo ">>> checking out and compiling dmplc"
 DMPL_BRANCH="$(jget -i input.json dmpl_branch)"
 lpwd=$PWD; cd $DMPL_ROOT; checkout_dmplc_branch $DMPL_BRANCH; make; cd $lpwd
