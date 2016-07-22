f270718f31ca33016d380782ca370f1c6e3a0a65
Sagar Chaki <chaki@sei.cmu.edu>
2016-07-22 10:41:47 -0400

    reindent. deleted unused imports. deleted code to find the location of instance file.

--- src/osate/org.osate.analysis.resource.dmpl.generator/src/org/osate/analysis/resource/dmpl/generator/actions/DMPLGeneratorAction.java
+++ src/osate/org.osate.analysis.resource.dmpl.generator/src/org/osate/analysis/resource/dmpl/generator/actions/DMPLGeneratorAction.java
@@ -5,7 +5,6 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.osate.aadl2.CalledSubprogram;
 import org.osate.aadl2.ComponentCategory;
@@ -22,124 +21,132 @@ import org.osate.aadl2.instance.SystemInstance;
 import org.osate.aadl2.instance.SystemOperationMode;
 import org.osate.aadl2.modelsupport.errorreporting.AnalysisErrorReporterManager;
 import org.osate.aadl2.modelsupport.modeltraversal.ForAllElement;
-import org.osate.aadl2.modelsupport.resources.OsateResourceUtil;
 import org.osate.aadl2.properties.PropertyNotPresentException;
 import org.osate.analysis.resource.zsrmscheduling.actions.ZSRMTask;
 import org.osate.ui.actions.AbstractInstanceOrDeclarativeModelReadOnlyAction;
 import org.osate.xtext.aadl2.properties.util.GetProperties;
 import org.osate.xtext.aadl2.properties.util.PropertyUtils;
 
-public class DMPLGeneratorAction extends AbstractInstanceOrDeclarativeModelReadOnlyAction {
+public class DMPLGeneratorAction extends AbstractInstanceOrDeclarativeModelReadOnlyAction
+{
 
-	@Override
-	protected void analyzeDeclarativeModel(IProgressMonitor monitor, AnalysisErrorReporterManager errManager,
-			Element declarativeObject) {
-	}
+  @Override
+  protected void analyzeDeclarativeModel(IProgressMonitor monitor, AnalysisErrorReporterManager errManager,
+                                         Element declarativeObject)
+  {
+  }
 
-	public String getDMPLTimingParametersFileName(ComponentInstance system) {
-		Property sourceNameProp = GetProperties.lookupPropertyDefinition(system, "DART", "DMPL_TIMING_PARAMETERS_TEXT");
-		String fileName = PropertyUtils.getStringValue(system, sourceNameProp);
-		return fileName;
-	}
+  public String getDMPLTimingParametersFileName(ComponentInstance system)
+  {
+    Property sourceNameProp = GetProperties.lookupPropertyDefinition(system, "DART", "DMPL_TIMING_PARAMETERS_TEXT");
+    String fileName = PropertyUtils.getStringValue(system, sourceNameProp);
+    return fileName;
+  }
 
-	public String getMainSubprogram(ComponentInstance threadInstance) {
-		String progName = "";
-		try {
-			Property entrycallsProperty = GetProperties.lookupPropertyDefinition(threadInstance,
-					"PROGRAMMING_PROPERTIES", "COMPUTE_ENTRYPOINT_CALL_SEQUENCE");
+  public String getMainSubprogram(ComponentInstance threadInstance)
+  {
+    String progName = "";
+    try {
+      Property entrycallsProperty = GetProperties.lookupPropertyDefinition(threadInstance,
+                                                                           "PROGRAMMING_PROPERTIES", "COMPUTE_ENTRYPOINT_CALL_SEQUENCE");
 
-			PropertyExpression propertyValue = threadInstance.getSimplePropertyValue(entrycallsProperty);
-			ReferenceValue callseqref = (ReferenceValue) propertyValue;
-			ContainmentPathElement element = callseqref.getContainmentPathElements().get(0);
-			SubprogramCallSequence cs = (SubprogramCallSequence) element.getNamedElement();
+      PropertyExpression propertyValue = threadInstance.getSimplePropertyValue(entrycallsProperty);
+      ReferenceValue callseqref = (ReferenceValue) propertyValue;
+      ContainmentPathElement element = callseqref.getContainmentPathElements().get(0);
+      SubprogramCallSequence cs = (SubprogramCallSequence) element.getNamedElement();
 
-			SubprogramCall call = cs.getOwnedSubprogramCalls().get(0);
+      SubprogramCall call = cs.getOwnedSubprogramCalls().get(0);
 
-			CalledSubprogram called = call.getCalledSubprogram();
-			Subprogram subprogram = (Subprogram) called;
+      CalledSubprogram called = call.getCalledSubprogram();
+      Subprogram subprogram = (Subprogram) called;
 
-			Property sourceNameProp = GetProperties.lookupPropertyDefinition(subprogram, "PROGRAMMING_PROPERTIES",
-					"SOURCE_NAME");
+      Property sourceNameProp = GetProperties.lookupPropertyDefinition(subprogram, "PROGRAMMING_PROPERTIES",
+                                                                       "SOURCE_NAME");
 
-			progName = PropertyUtils.getStringValue(subprogram, sourceNameProp);
-		} catch (PropertyNotPresentException pne) {
-			System.out.println(threadInstance.getName() + " has no Source_Name property");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+      progName = PropertyUtils.getStringValue(subprogram, sourceNameProp);
+    } catch (PropertyNotPresentException pne) {
+      System.out.println(threadInstance.getName() + " has no Source_Name property");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
-		return progName;
-	}
+    return progName;
+  }
 
-	ArrayList<ZSRMTask> tasks = new ArrayList<ZSRMTask>();
-	ArrayList<ComponentInstance> processes = new ArrayList<ComponentInstance>();
-	HashMap<ComponentInstance, ArrayList<ZSRMTask>> proc2taskset = new HashMap<ComponentInstance, ArrayList<ZSRMTask>>();
+  ArrayList<ZSRMTask> tasks = new ArrayList<ZSRMTask>();
+  ArrayList<ComponentInstance> processes = new ArrayList<ComponentInstance>();
+  HashMap<ComponentInstance, ArrayList<ZSRMTask>> proc2taskset = new HashMap<ComponentInstance, ArrayList<ZSRMTask>>();
 
-	protected void analyzeInstanceModel(IProgressMonitor monitor, final AnalysisErrorReporterManager errManager,
-			SystemInstance root, SystemOperationMode som) {
+  protected void analyzeInstanceModel(IProgressMonitor monitor,
+                                      final AnalysisErrorReporterManager errManager,
+                                      SystemInstance root, SystemOperationMode som)
+  {
+    tasks.clear();
+    processes.clear();
+    proc2taskset.clear();
 
-		//IResource is = OsateResourceUtil.convertToIResource(root.eResource());
-		
-		tasks.clear();
-		processes.clear();
-		proc2taskset.clear();
+    final ForAllElement addProcesses = new ForAllElement(errManager) {
+        public void process(Element obj) {
+          ComponentInstance procinst = (ComponentInstance) obj;
+          processes.add(procinst);
+          final ArrayList<ZSRMTask> taskset = new ArrayList<ZSRMTask>();
+          proc2taskset.put(procinst, taskset);
+          final ForAllElement addTasks = new ForAllElement(errManager) {
+              public void process(Element obj) {
+                ComponentInstance ci = (ComponentInstance) obj;
+                final ZSRMTask task = ZSRMTask.createInstance(ci);
+                task.fillZeroSlackInstant();
+                tasks.add(task);
+                taskset.add(task);
+              }
+            };
+          addTasks.processPreOrderComponentInstance(procinst, ComponentCategory.THREAD);
+        }
+      };
 
-		final ForAllElement addProcesses = new ForAllElement(errManager) {
-			public void process(Element obj) {
-				ComponentInstance procinst = (ComponentInstance) obj;
-				processes.add(procinst);
-				final ArrayList<ZSRMTask> taskset = new ArrayList<ZSRMTask>();
-				proc2taskset.put(procinst, taskset);
-				final ForAllElement addTasks = new ForAllElement(errManager) {
-					public void process(Element obj) {
-						ComponentInstance ci = (ComponentInstance) obj;
-						final ZSRMTask task = ZSRMTask.createInstance(ci);
-						task.fillZeroSlackInstant();
-						tasks.add(task);
-						taskset.add(task);
-					}
-				};
-				addTasks.processPreOrderComponentInstance(procinst, ComponentCategory.THREAD);
-			}
-		};
-		addProcesses.processPreOrderComponentInstance(root, ComponentCategory.PROCESS);
+    addProcesses.processPreOrderComponentInstance(root, ComponentCategory.PROCESS);
 
-		String filename = getDMPLTimingParametersFileName(root);
+    String filename = getDMPLTimingParametersFileName(root);
 
-		try {
-			PrintWriter pw = new PrintWriter(filename);
-			for (ComponentInstance process : processes) {
-				pw.println("NODE  uav"); // + process.getContainingComponentInstance().getName());
-				pw.println("{");
-				ArrayList<ZSRMTask> taskset = proc2taskset.get(process);
-				// for (ZSRMTask t : tasks) {
-				for (ZSRMTask t : taskset) {
-					ComponentInstance instance = t.getComponentInstance();
-					String progName = getMainSubprogram(instance);
-					// pw.println("@PERIOD(" + t.getPeriodNanos() + ")");
-					pw.println("@HERTZ(" + (int) (1000000000.0 / (t.getPeriodNanos() * 1.0)) + ")");
-					pw.println("@CRITICALITY(" + t.getCriticality() + ")");
-					pw.println("@WCET_NOMINAL(" + (t.getNominalWCETNanos() / 1000.0) + ")");
-					pw.println("@WCET_OVERLOAD(" + (t.getOverloadedWCETNanos() / 1000.0) + ")");
-					pw.println("@ZERO_SLACK_INSTANT(" + (t.getZeroSlackInstantNanos() / 1000.0) + ")");
-					pw.println(progName + "();");
-					pw.println(" ");
-				}
-				pw.println("}");
+    try {
+      PrintWriter pw = new PrintWriter(filename);
+      for (ComponentInstance process : processes) {
+        pw.println("NODE  uav"); // + process.getContainingComponentInstance().getName());
+        pw.println("{");
+        ArrayList<ZSRMTask> taskset = proc2taskset.get(process);
+        // for (ZSRMTask t : tasks) {
+        for (ZSRMTask t : taskset) {
+          ComponentInstance instance = t.getComponentInstance();
+          String progName = getMainSubprogram(instance);
+          // pw.println("@PERIOD(" + t.getPeriodNanos() + ")");
+          pw.println("@HERTZ(" + (int) (1000000000.0 / (t.getPeriodNanos() * 1.0)) + ")");
+          pw.println("@CRITICALITY(" + t.getCriticality() + ")");
+          pw.println("@WCET_NOMINAL(" + (t.getNominalWCETNanos() / 1000.0) + ")");
+          pw.println("@WCET_OVERLOAD(" + (t.getOverloadedWCETNanos() / 1000.0) + ")");
+          pw.println("@ZERO_SLACK_INSTANT(" + (t.getZeroSlackInstantNanos() / 1000.0) + ")");
+          pw.println(progName + "();");
+          pw.println(" ");
+        }
+        pw.println("}");
 
-				// **** HACK !!!!!!!*******
-				// For now we only generate the first node
-				break;
-			}
-			pw.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("Filename [" + filename + "] not valid");
-		}
+        // **** HACK !!!!!!!*******
+        // For now we only generate the first node
+        break;
+      }
+      pw.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("Filename [" + filename + "] not valid");
+    }
 
-	}
+  }
 
-	protected String getActionName() {
-		return "DMPLGenerator";
-	}
+  protected String getActionName()
+  {
+    return "DMPLGenerator";
+  }
 
 }
+
+/*********************************************************************/
+//-- end of file
+/*********************************************************************/
